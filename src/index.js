36a735972aa61deb639b3794cc766745310a26ad
Jasmine Carter <jasmine.carter87@gmail.com>
2020-12-02 00:57:08 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ function logSubmit(event) {
     log.textContent = `test`;
         event.preventDefault();
 }
-const form = document.getElementbyId('form');
-const log = document.getElementbyId('log');
+const form = document.getElementById('form');
+const log = document.getElementById('log');
 form.addEventListener('submit', logSubmit);
 });
   // your code here
