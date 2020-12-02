b4adb7c55693867d8b1e4b73cd51e376d36c70ff
Jasmine Carter <jasmine.carter87@gmail.com>
2020-12-02 00:24:04 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-  let input = document.getElementById("new-task-description")
+  let input = document.getElementById("new-task-description").value
   document.querySelector("input[type=submit]").addEventListener("click", function(event) {
     document.getElementById("tasks").appendChild(input);
   })
