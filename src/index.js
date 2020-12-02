2514b251b185451ca27aadc7f2e30637826133e9
Jasmine Carter <jasmine.carter87@gmail.com>
2020-12-02 00:56:56 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -13,16 +13,3 @@ const log = document.getElementbyId('log');
 form.addEventListener('submit', logSubmit);
 });
   // your code here
-
-
-
-
-  let input = document.getElementById("new-task-description").value
-  const ul = document.getElementById("tasks")
-  let li = document.createElement("li")
-  li.innerText = input
-  document.querySelector("input[type=submit]").addEventListener("click", function(event) {
-    ul.appendChild(li);
-    event.preventDefault();
-  })
-});
