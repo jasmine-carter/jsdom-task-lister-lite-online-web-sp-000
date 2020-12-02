a3af5c56431359be54a9aaf09791434d74284fe3
Jasmine Carter <jasmine.carter87@gmail.com>
2020-12-02 00:21:05 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let input = document.getElementById("new-task-description")
+  document.querySelector("input[type=submit]").addEventListener("click", function(event) {
+    document.getElementById("tasks").appendChild(input);
+  })
 });
