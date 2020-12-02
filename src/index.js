6f45c6ce6e51cfc7b79aaa69c01e5fabfa3d51f2
Jasmine Carter <jasmine.carter87@gmail.com>
2020-12-02 01:14:40 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -4,12 +4,11 @@ let input = document.getElementById("new-task-description").value
 const taskUl = document.getElementById("tasks")
 let li = document.createElement("li")
 
-function logSubmit(event) {
-    log.textContent = `test`;
-        event.preventDefault();
-}
-const form = document.getElementById('form');
-const log = document.getElementById('log');
-form.addEventListener('submit', logSubmit);
+document.querySelector("input[type=submit]").addEventListener("click", function(event){
+  li.textContent = input
+  taskUl.appendChild(li);
+}, false);
+
+event.preventDefault()
 });
   // your code here
