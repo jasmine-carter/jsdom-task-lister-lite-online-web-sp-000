8cae854b34fc4a5705dc467fd4f6b24049da3dcd
Jasmine Carter <jasmine.carter87@gmail.com>
2020-12-02 00:55:29 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
+
+let input = document.getElementById("new-task-description").value
+const taskUl = document.getElementById("tasks")
+let li = document.createElement("li")
+
+function logSubmit(event) {
+    log.textContent = `test`;
+        event.preventDefault();
+}
+const form = document.getElementbyId('form');
+const log = document.getElementbyId('log');
+form.addEventListener('submit', logSubmit);
+});
   // your code here
+
+
+
+
   let input = document.getElementById("new-task-description").value
   const ul = document.getElementById("tasks")
   let li = document.createElement("li")
