f2c1325f6b78975c8805b4425741e6f4df29d50d
Jasmine Carter <jasmine.carter87@gmail.com>
2020-12-02 01:20:37 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-let input = document.getElementById("new-task-description").value
 const taskUl = document.getElementById("tasks")
 let li = document.createElement("li")
 
 document.querySelector("input[type=submit]").addEventListener("click", function(event){
+  let input = document.getElementById("new-task-description").value
   li.textContent = input
   taskUl.appendChild(li);
   event.preventDefault()
